f0ced1e51723eb21740873b423692fca30e6e47b
chongoctoan <chongoctoan@gmail.com>
2022-04-06 17:31:56 +0700

    btvn 4-4-2022

---
+++ btvn 4-4-2022/job1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("nhap a="); scanf("%d",&a);
+	
+	if(a>=2&&a<8){
+		printf("day la thu:%d",a);
+		 
+	}else{
+		if(a!=8){
+			printf("khong phai ngay trong tuan"); 
+		}else{
+			printf("day la chu nhat"); 
+		} 
+	} 
+} 
