b289502ffa722f94cb4b0d22a5636514b2607928
chongoctoan <chongoctoan@gmail.com>
2022-04-27 19:43:10 +0700

    PRACTICE EXAM

---
+++ bai thi/bai2.cpp
@@ -0,0 +1,21 @@
+# include<stdio.h>
+void reverseArray(int ary[], int n){
+ for(int i=0;i<n;i++){
+  	printf("\n Phan tu thu %d ",i);
+  	scanf("%d",&ary[i]);
+ }
+ 
+ 	printf("Reverse Integer Array \n");
+ 
+ for(int i=n-1;i>=0;i--){
+  	printf("%d  ", ary[i]);
+ }
+ 
+}
+int main(){
+ int n;
+ printf("Nhap vao so luong cac phan tu n ");
+ scanf("%d",&n);
+ int ary[n];
+ reverseArray(ary,n);
+}
